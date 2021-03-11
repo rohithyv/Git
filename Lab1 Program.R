a295b0bb48ec71614ad098a634e5cfc4eef94f3d
rohithyv <rohithyv>
2021-03-11 18:14:05 +0530

    added for learning purpose

---
+++ Lab1 Program.R
@@ -0,0 +1,6 @@
+#1)Write a R code to create a list of data objects of type numeric,string,Real numbers and name them
+N1<-c(10,30,50)
+S1<-c("Water","lemon","Ram")
+R1<-c(20.1,45.5,12.33)
+L1<-list(N1,S1,R1)
+L1
---
+++ Lab2 Program.R
@@ -0,0 +1,14 @@
+#this is a demo for github
+
+#2)Make Five simple numeric vectors of the data
+
+BuffTail<-c(10,1,37,5,12)
+GardenBee<-c(8,3,9,6,4)
+RedTail<-c(18,9,12,4,6)
+CarderBee<-c(8,27,6,32,23)
+HoneyBee<-c(12,13,16,9,10)
+beedataFrame<-data.frame(BuffTail,GardenBee,RedTail,CarderBee,HoneyBee)
+beedataFrame
+plants<-c("Thistle","Vipers","GoldenRain","Yellowfala","Blackberry")
+rownames(beedataFrame)<-plants
+beedataFrame
